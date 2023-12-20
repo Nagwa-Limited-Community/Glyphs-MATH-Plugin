5c446b01c2fbeb6212c8b408ee1d0622fd5f55c6
schriftgestalt <georg.seifert@mac.com>
2023-12-20 09:16:22 +0100

    add basic explanation on how to update .strings file

--- MATHPlugin.glyphsPlugin/Contents/Resources/plugin.py
+++ MATHPlugin.glyphsPlugin/Contents/Resources/plugin.py
@@ -257,6 +257,16 @@ pluginBundle = None
 path = __file__[:__file__.rfind("Contents/Resources/")]
 pluginBundle = AppKit.NSBundle.bundleWithPath_(path)
 
+"""
+    when you add more `NSLocalizedString()`, run this from the command line (with the Resources folder as current folder)
+    genstrings -u -q -o en.lproj plugin.py
+
+    (requires macOS and Xcode installed.)
+    For now, it needs the `def NSLocalizedString()` be changed to something like `def NSLocalizedStringXX()` to not confuse genstrings
+
+    then sync all the new keys with the localized files.
+"""
+
 def NSLocalizedString(string, comment):
     return pluginBundle.localizedStringForKey_value_table_(string, string, None)
 
